8ba22728f56cbac92eeb8029e1de368dff9af2dd
BazBaz1337 <ilhanbazli@gmail.com>
2026-09-19 01:38:15 +0800

    update 0.4:
    scrape entire name_list List
    Created a main() function that will have the List , Dictionary and Rate functions

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ def get_positive_float(prompt):
 def main():
     print("=== Exercise 2 : Itemized Receipt Splitter ===")
     #Get names and store in a list in nameList
-
+    names = input("Please enter your names (seperated by commas): ")
+    nameList = [name.strip() for name in names.split(',') if name.strip()]
+    if not nameList:
+        print("No participants entered. Exiting.")
+        return
+    #print(nameList) test nameList list array
     #Get items and prices of items and store in a dictionary orderedItems
 
     #Get tax% and tip%
